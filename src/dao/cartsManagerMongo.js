fbd919b1b0dbd4a00f2678b814733bd12ec0b78e
Leandro Schlain <cokinio@gmail.com>
2023-04-15 12:52:53 -0300

    se creo correctament cart con mongoose

--- src/dao/cartsManagerMongo.js
+++ src/dao/cartsManagerMongo.js
@@ -1,5 +1,6 @@
-import {cartsModel} from "../models/carts.model.js"
-import {productsModel} from "../models/products.models.js";
+import { cartsModel } from "../models/carts.model.js";
+import { productsModel } from "../models/products.models.js";
+import mongoose from 'mongoose';
 
 export default class CartManager {
 	static cuentaGlobal = 0;
@@ -10,36 +11,36 @@ export default class CartManager {
 
 	//Metodos
 	async NewCart(products1) {
-	
-		let thereAreNonExistingProducts=false;
+		let thereAreNonExistingProducts = false;
+		let NonExistentProduct;
 
 		if (products1 !== undefined) {
-			
 			//check if all the products exists
 
 			await products1.reduce(async (e) => {
-				console.log(e.pid)
-				let productExists= await this.getProductById(e.pid);
-				console.log(productExists)
-				if (productExists!== true ){
+				console.log(e.pid);
+				let productExists = await this.getProductById(e.pid);
+				console.log(productExists);
+				if (productExists !== true) {
 					console.error(`Product ${e.pid} does not exists`);
-					thereAreNonExistingProducts=true;
-					NonExistentProduct=e.pid;
-
+					thereAreNonExistingProducts = true;
+					NonExistentProduct = e.pid;
 				}
 			});
 
-			
-			if (thereAreNonExistingProducts===true){
+			if (thereAreNonExistingProducts === true) {
 				return [false, `Product ${NonExistentProduct} does not exists`];
 			}
 
 			//create a cart
-			let cart= await cartsModel.create({});
-			console.log(cart)
+			let cart = await cartsModel.create({});
 			//write products in the file
-			cart.products.push({products1});
-			let result = cartsModel.updateOne({_id:cart._id,cart})
+			cart.products = products1;
+			//cart.products.push({"pid":"6439933231c5c1c7aa5f2516","quant":2})
+			let result = await cartsModel.updateOne({ _id: cart._id }, cart);
+			//let carrito= await cartsModel.find({_id:cart._id}).populate('products.pid');
+			let carrito = await cartsModel.find({ _id: cart._id });
+			console.log(JSON.stringify(carrito, null, "\t"));
 			return [true, cart._id];
 		} else {
 			console.log("missing input parameters");
@@ -48,109 +49,109 @@ export default class CartManager {
 	}
 
 	async getCartById(idBuscado) {
-		let jsonString = await fs.promises.readFile(fileName, "utf-8");
-		let products = JSON.parse(jsonString);
-		let busqueda = products.filter((e) => e.id == parseInt(idBuscado));
-		if (busqueda.length > 0) {
-			console.log("the cart searched is the following:");
-			console.log(busqueda)
-			return busqueda;
-		} else {
-			console.log("cart not found");
-			return null;
+		try {
+			let cart = await cartsModel.find({ _id: idBuscado });
+			console.log(`the cart searched is the following: ${cart}`);
+			return cart;
+		} catch (error) {
+			console.log(
+				`No se pudo obtener el cart ${idBuscado} con moongose: ` + error
+			);
+			return false;
 		}
 	}
 
-	async addProductToCart(cart1,product1, quantity1) {
-		let searchedIndex=0;
-		let searchedCart=[];
-		if (product1 !== undefined && cart1!== undefined && quantity1!== undefined) {
+	async addProductToCart(cart1, product1, quantity1) {
+		let searchedIndex = 0;
+		let searchedCart = [];
+		if (
+			product1 !== undefined &&
+			cart1 !== undefined &&
+			quantity1 !== undefined
+		) {
 			//check if the productId is valid
-			let productExists= await this.getProductById(product1);
-			if (productExists===false){
+			let productExists = await this.getProductById(product1);
+			console.log(productExists)
+			if (productExists === false) {
 				console.log("Non existent product");
 				return [false, "Non existent product"];
 			}
 
-			// check if the file is already created
-			if (!fs.existsSync(fileName)) {
-				console.error(
-					"The file doesnt exists, we will create one for you"
-				);
-				let initialData = JSON.stringify([]);
-				await fs.promises.writeFile(fileName, initialData);
-			}
-			// read fileName in order to search if the cart is stored in the file
-			let jsonString = await fs.promises.readFile(fileName, "utf-8");
-			let carts = JSON.parse(jsonString);
-			let cartsLength=carts.length;
-			if (cartsLength>0){
-				
-				//search for the index of the cart id
-				searchedIndex = carts.findIndex((e) => e.id == cart1);
-				searchedCart=carts[searchedIndex];
-				
-				//search if the cart exists
-				if (searchedIndex != -1){
-					//search if the product is already in the cart
-					let productsInCart=searchedCart.products;
-					console.log(productsInCart)
-					let searchedProduct = productsInCart.findIndex((e) => e.pid ==product1 );
-						//the product is already in the cart
-						console.log(searchedProduct)
-						if (searchedProduct != -1){
-							console.log(productsInCart[searchedProduct])
-							carts[searchedIndex].products[searchedProduct].quant++;
-							console.log(productsInCart[searchedProduct])
-						}else{
-						searchedCart.products.push({pid:parseInt(product1), quant:quantity1})
-						}
-					
-				}else{
-					console.log("Non existent cart");
-					return [false, "Non existent cart"];
+			//search if the cart exists
+			let searchedCart = await this.getCartById(cart1);
+			if (searchedCart != false) {
+				//search if the product is already in the cart
+				//let searchedProduct = this.addProductInCart(searchedCart, product1);
+				let product = await cartsModel.find({ _id: cart1, "products.pid":{_id: product1} });
+				console.log(product)
+				 if (product != false) {
+				// 	console.log(productsInCart[searchedProduct]);
+				// 	carts[searchedIndex].products[searchedProduct].quant++;
+				// 	console.log(productsInCart[searchedProduct]);
+				// } else {
+				// 	searchedCart.products.push({
+				// 		pid: parseInt(product1),
+				// 		quant: quantity1,
+				// 	});
+				return [false, "Non existent cart"];
 				}
-				
-			}else{
-				CartManager.cuentaGlobal++;
-				searchedCart.id = CartManager.cuentaGlobal;
-				searchedCart.products.push([{pid:product1, quant:quantity1}]);
+			} else {
+				console.log("Non existent cart");
+				return [false, "Non existent cart"];
 			}
-			
-			carts[searchedIndex]=searchedCart;
-			//write products in the file
-			let data = JSON.stringify(carts);
-			await fs.promises.writeFile(fileName, data);
-			console.log("product succesfully added");
-			return [true, cart1.id];
 		} else {
 			console.log("missing input parameters");
 			return [false, "missing input parameters"];
 		}
 	}
 
+	//lo uso para buscar si el producto existe
 	async getProductById(idBuscado) {
 		try {
-			let product = await productsModel.find({_id:idBuscado})
+			let product = await productsModel.find({ _id: idBuscado });
 			console.log(`the product searched is the following: ${product}`);
-			return true;
-			}
-			catch(error) {
-				console.log(`No se pudo obtener el producto ${idBuscado} con moongose: `+ error);
+			if (product!=={}){
+				return true;
+			}else{
 				return false;
 			}
+		} catch (error) {
+			console.log(
+				`No se pudo obtener el producto ${idBuscado} con moongose: ` + error
+			);
+			return false;
+		}
+	}
+
+	async addProductInCart(cart, productID) {
+		try {
+			let product = cart.products.find((producto) => producto.pid === productID);
+			console.log(product);
+			if (product !== {}) {
+				console.log(`the product searched is the following: ${product}`);
+				return product;
+			} else {
+				console.log(`the product searched is contained in the cart`);
+				return false;
+			}
+		}
+		catch (error) {
+			console.log(
+				`No se pudo obtener el producto ${productID} con moongose: ` + error
+			);
+			return false;
+		}
 	}
 }
-	
-let testing= async ()=> {
+
+let testing = async () => {
 	let cartManager1 = new CartManager("./");
 	// await cartManager1.NewCart([{pid:1,quant:2},{pid:12,quant:4}]);
 	// await cartManager1.NewCart([{pid:2,quant:2},{pid:12,quant:4}]);
 	// await cartManager1.NewCart([{pid:2,quant:2},{pid:12,quant:4}]);
 	// await cartManager1.NewCart([{pid:8,quant:3},{pid:9,quant:5}]);
 	// await cartManager1.getCartById(2);
-	await cartManager1.addProductToCart(3,5,1)
-}	
-		
+	await cartManager1.addProductToCart(3, 5, 1);
+};
 
 //testing()
--- src/models/carts.model.js
+++ src/models/carts.model.js
@@ -2,18 +2,35 @@ import mongoose from 'mongoose';
 
 const collectionName = 'carts';
 
-const courseSchema = new mongoose.Schema({
-    products: {
-        type:[
-            {
-                product:{
+const stringTypeSchemaUniqueRequired = {
+    type: String,
+    unique: true,
+    required: true
+};
+
+const numberTypeSchemaNonUniqueRequired = {
+    type: Number,
+    required: true
+};
+
+const typePid ={
+                 pid:{
                     type:mongoose.Schema.Types.ObjectId,
-                    ref:"product"
-                }
-            }
-        ],
-        default:[]
-    }
+                     ref:"products",
+                 },
+                 quant:numberTypeSchemaNonUniqueRequired
+             }
+
+
+const courseSchema = new mongoose.Schema({
+
+    products: [
+        {
+        type:typePid,
+        
+        }
+    ]
+    
 });
 
 export const cartsModel = mongoose.model(collectionName, courseSchema);
