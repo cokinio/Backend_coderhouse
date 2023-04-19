f470fa01d1a42dc16b4a749e7420513c05a4df61
Leandro Schlain <cokinio@gmail.com>
2023-04-19 15:50:58 -0300

    avances varios

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -12,10 +12,10 @@ router.get('/', async (req, res)=>{
 })
 
 router.get('/realtimeproducts', async (req, res)=>{
-    let products = await productManager1.getProducts();
-    res.render('realTimeProducts', {
-        products
-    })
+    let { limit,page } = req.query;
+    page = parseInt(req.query.page);
+    let products = await productManager1.getProducts(limit,page);
+    res.render('realTimeProducts',products)
 })
 
 router.get('/chat', async (req, res)=>{
