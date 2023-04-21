deaf6c063b662c3f71de94a57f06a37be271c9aa
Leandro Schlain <cokinio@gmail.com>
2023-04-21 16:15:43 -0300

    se agregaron endpoints

--- src/dao/cartsManagerMongo.js
+++ src/dao/cartsManagerMongo.js
@@ -10,37 +10,43 @@ export default class CartManager {
 	}
 
 	//Metodos
-	async NewCart(products1) {
-		console.log(products1);
+	async NewCart(products1, cartId) {
+		//console.log(products1);
+		console.log(cartId);
 		let thereAreNonExistingProducts = 0;
 		let NonExistentProduct;
+		let cart = {};
 		if (products1 !== undefined) {
 			//check if all the products exists
-			let thereAreNonExistingProducts= await Promise.all(products1.map(async (e) => {
-				let productExists = await this.getProductById(e.pid);
-				return productExists;
-			}));
-			console.log(thereAreNonExistingProducts)
+			let thereAreNonExistingProducts = await Promise.all(
+				products1.map(async (e) => {
+					let productExists = await this.getProductById(e.pid);
+					return productExists;
+				})
+			);
+			console.log(thereAreNonExistingProducts);
 			if (thereAreNonExistingProducts.includes(false)) {
-				let indice= thereAreNonExistingProducts.indexOf(false);
-				let id= products1[indice].pid;
+				let indice = thereAreNonExistingProducts.indexOf(false);
+				let id = products1[indice].pid;
 				console.log(`Product ${id} does not exists`);
 				return [false, `Product ${id} does not exists`];
 			} else {
 				//create a cart
-				let cart = await cartsModel.create({});
+				let idCart = cartId;
+				if (cartId === undefined) {
+					cart = await cartsModel.create({});
+					idCart = cart._id;
+				}
+
 				//write products in the file
 				cart.products = products1;
-				let result = await cartsModel.updateOne(
-					{ _id: cart._id },
-					cart
-				);
+				let result = await cartsModel.updateOne({ _id: idCart }, cart);
 				let carrito = await cartsModel
 					.find({ _id: cart._id })
 					.populate("products.pid");
 				//let carrito = await cartsModel.find({ _id: cart._id });
 				console.log(JSON.stringify(carrito, null, "\t"));
-				return [true, cart._id];
+				return [true, idCart];
 			}
 		} else {
 			console.log("missing input parameters");
@@ -86,7 +92,7 @@ export default class CartManager {
 					searchedCart,
 					product1,
 					quantity1,
-					true
+					1
 				);
 				let result = await productAlreadyIncreasedInCart;
 				if (result !== false) {
@@ -142,7 +148,137 @@ export default class CartManager {
 		}
 	}
 
-	async addProductInCart(cart, productID, quantity, sumar) {
+	async deleteProductfromCart(cart1, product1, quantity1) {
+		let searchedIndex = 0;
+		let searchedCart = [];
+		if (
+			product1 !== undefined &&
+			cart1 !== undefined &&
+			quantity1 !== undefined
+		) {
+			//check if the productId is valid
+			let productExists = await this.getProductById(product1);
+			console.log(productExists);
+			if (productExists === false) {
+				console.log("Non existent product");
+				return [false, "Non existent product"];
+			}
+			//search if the cart exists
+			let searchedCart = await this.getCartById(cart1);
+			if (searchedCart != false) {
+				//search if the product is already in the cart
+				let productAlreadyIncreasedInCart = this.addProductInCart(
+					searchedCart,
+					product1,
+					quantity1,
+					2
+				);
+				let result = await productAlreadyIncreasedInCart;
+				if (result !== false) {
+					//I already have quant decreased by one
+					console.log(result);
+					let updated = await cartsModel.updateOne(
+						{ _id: cart1 },
+						result
+					);
+					console.log(updated);
+					return [true, cart1];
+				} else {
+				}
+			} else {
+				console.log("Non existent cart");
+				return [false, "Non existent cart"];
+			}
+		} else {
+			console.log("missing input parameters");
+			return [false, "missing input parameters"];
+		}
+	}
+
+	async deleteCartProducts(cart1) {
+		let searchedIndex = 0;
+		let searchedCart = [];
+		try {
+			if (cart1 !== undefined) {
+				//search if the cart exists
+				let searchedCart = await this.getCartById(cart1);
+				if (searchedCart != false) {
+					searchedCart.products = [];
+					let updated = await cartsModel.updateOne(
+						{ _id: cart1 },
+						searchedCart
+					);
+					console.log(cart1);
+					return [true, cart1];
+				} else {
+					console.log("Non existent cart");
+					return [false, "Non existent cart"];
+				}
+			} else {
+				console.log("missing input parameters");
+				return [false, "missing input parameters"];
+			}
+		} catch (error) {
+			console.log(
+				`No se pudo obtener el carrito ${cart1} con moongose: ` +
+					error
+			);
+			return false;
+		}
+	}
+
+
+async updateProductInCart(cart1, product1, quantity1) {
+	let searchedIndex = 0;
+	let searchedCart = [];
+	if (
+		product1 !== undefined &&
+		cart1 !== undefined &&
+		quantity1 !== undefined
+	) {
+		//check if the productId is valid
+		console.log(product1)
+		typeof(product1)
+		let productExists = await this.getProductById(product1);
+		console.log(productExists);
+		if (productExists === false) {
+			console.log("Non existent product");
+			return [false, "Non existent product"];
+		}
+		//search if the cart exists
+		let searchedCart = await this.getCartById(cart1);
+		if (searchedCart != false) {
+			//search if the product is already in the cart
+			let productAlreadyUpdatedInCart = this.addProductInCart(
+				searchedCart,
+				product1,
+				parseInt(quantity1),
+				3
+			);
+			let result = await productAlreadyUpdatedInCart;
+			if (result !== false) {
+				//I already have quant decreased by one
+				console.log(result);
+				let updated = await cartsModel.updateOne(
+					{ _id: cart1 },
+					result
+				);
+				console.log(updated);
+				return [true, cart1];
+			} else {
+			}
+		} else {
+			console.log("Non existent cart");
+			return [false, "Non existent cart"];
+		}
+	} else {
+		console.log("missing input parameters");
+		return [false, "missing input parameters"];
+	}
+}
+
+
+async addProductInCart(cart, productID, quantity, operacion) {
 		try {
 			console.log(`el cart pasado a addProductInCart es: ${cart}`);
 			console.log(typeof cart);
@@ -156,24 +292,43 @@ export default class CartManager {
 			});
 			console.log(resultado);
 			if (resultado !== -1) {
-				if (sumar){
+				// operacion 1 =sumar
+				// operacion 2 = restar
+				// operacion 3 = actualizar cantidad
+				if (operacion===1) {
 					cart.products[resultado].quant =
 						cart.products[resultado].quant + quantity;
 					console.log(typeof cart);
 					return cart;
-				}else{ //estoy restando
-					if (cart.products[resultado].quant>0){
-						cart.products[resultado].quant=cart.products[resultado].quant - quantity;
+				} else if (operacion===2) {
+					//estoy restando
+					if (cart.products[resultado].quant > 0) {
+						cart.products[resultado].quant =
+							cart.products[resultado].quant - quantity;
 						//si me quedo un numero negativo hago que sea cero
-						cart.products[resultado].quant<0 ? cart.products[resultado].quant=0 :  cart.products[resultado].quant=cart.products[resultado].quant;
+						cart.products[resultado].quant < 0
+							? (cart.products[resultado].quant = 0)
+							: (cart.products[resultado].quant =
+									cart.products[resultado].quant);
 						//si es cero la cantidad borro el producto del arreglo
-						if (cart.products[resultado].quant===0) {cart.products.splice(resultado,1)};
+						if (cart.products[resultado].quant === 0) {
+							cart.products.splice(resultado, 1);
+						}
 						return cart;
-					}else{
-						console.log("entre splice")
-						cart.products.splice(resultado,1);
+					} else {
+						console.log("entre splice");
+						cart.products.splice(resultado, 1);
 						return cart;
 					}
+				}else if (operacion===3) {
+					console.log("entre en operacion 3")
+					cart.products[resultado].quant = quantity;
+					return cart;
+				}else{
+					console.log(
+						`la operacion solitada no existe`
+					);
+					return false;
 				}
 			} else {
 				console.log(
@@ -189,54 +344,6 @@ export default class CartManager {
 			return false;
 		}
 	}
-
-
-async deleteProductfromCart(cart1, product1, quantity1) {
-	let searchedIndex = 0;
-	let searchedCart = [];
-	if (
-		product1 !== undefined &&
-		cart1 !== undefined &&
-		quantity1 !== undefined
-	) {
-		//check if the productId is valid
-		let productExists = await this.getProductById(product1);
-		console.log(productExists);
-		if (productExists === false) {
-			console.log("Non existent product");
-			return [false, "Non existent product"];
-		}
-		//search if the cart exists
-		let searchedCart = await this.getCartById(cart1);
-		if (searchedCart != false) {
-			//search if the product is already in the cart
-			let productAlreadyIncreasedInCart = this.addProductInCart(
-				searchedCart,
-				product1,
-				quantity1,
-				false
-			);
-			let result = await productAlreadyIncreasedInCart;
-			if (result !== false) {
-				//I already have quant decreased by one
-				console.log(result);
-				let updated = await cartsModel.updateOne(
-					{ _id: cart1 },
-					result
-				);
-				console.log(updated);
-				return [true, cart1];
-			}else {
-			}
-		} else {
-			console.log("Non existent cart");
-			return [false, "Non existent cart"];
-		}
-	} else {
-		console.log("missing input parameters");
-		return [false, "missing input parameters"];
-	}
-}
 
 }
 
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -5,6 +5,16 @@ const router = Router();
 
 export let cartManager1 = new CartManager("./");
 
+router.get("/:cid", async (req,res)=>{
+    console.log(req.params.cid);
+	let cartById = await cartManager1.getCartById(req.params.cid);
+	if (cartById != null) {
+		res.send(cartById);
+	} else {
+		res.send(`el cart con ID ${req.params.cid} no existe`);
+	}
+})
+
 router.post("/",async (req,res)=>{
     let products=req.body;
     console.log(products)
@@ -16,25 +26,24 @@ router.post("/",async (req,res)=>{
     }
 })
 
-router.get("/:cid", async (req,res)=>{
-    console.log(req.params.cid);
-	let cartById = await cartManager1.getCartById(req.params.cid);
-	if (cartById != null) {
-		res.send(cartById);
-	} else {
-		res.send(`el cart con ID ${req.params.cid} no existe`);
-	}
-})
-
-
 router.post("/:cid/product/:pid",async (req,res)=>{
-    let products=req.body;
-    console.log(products);
+    // let products=req.body;
+    // console.log(products);
     let wasProductAddedSuccesfully = await cartManager1.addProductToCart(req.params.cid,req.params.pid,1);
     if (wasProductAddedSuccesfully[0]===true) {
         res.send({ status: "Success", message: `Productos agregados con exito al carrito`});
     } else{
         res.status(400).send({ status: "Error", message: wasProductAddedSuccesfully[1] });
+    }
+})
+
+router.delete("/:cid",async (req,res)=>{
+    let cart=req.params.cid;
+    let wasProductsDeleted = await cartManager1.deleteCartProducts(cart);
+    if ( wasProductsDeleted[0]===true) {
+        res.send({ status: "Success", message: `Productos eliminados con exito del carrito con ID:${wasProductsDeleted[1]}`});
+    } else{
+        res.status(400).send({ status: "Error", message: wasProductsDeleted[1] });
     }
 })
 
@@ -48,5 +57,29 @@ router.delete("/:cid/product/:pid",async (req,res)=>{
         res.status(400).send({ status: "Error", message: wasProductDeletedSuccesfully[1] });
     }
 })
+
+router.put("/:cid",async (req,res)=>{
+    let cart=req.params.cid;
+    let products=req.body;
+    console.log(products)
+    let wasCartAddedSuccesfully = await cartManager1.NewCart(products,cart);
+    if (wasCartAddedSuccesfully[0]===true) {
+        res.send({ status: "Success", message: `Productos actualizados con exito al carrito con ID:${wasCartAddedSuccesfully[1]}`});
+    } else{
+        res.status(400).send({ status: "Error", message: wasCartAddedSuccesfully[1] });
+    }
+})
+
+router.put("/:cid/product/:pid",async (req,res)=>{
+    let productCant=req.body[0].quant;
+    console.log(productCant);
+    let wasProductUpdatedSuccesfully = await cartManager1.updateProductInCart(req.params.cid,req.params.pid,productCant);
+    if (wasProductUpdatedSuccesfully[0]===true) {
+        res.send({ status: "Success", message: `Producto actualizado con exito al carrito`});
+    } else{
+        res.status(400).send({ status: "Error", message: wasProductUpdatedSuccesfully[1] });
+    }
+})
+
 
 export default router;
