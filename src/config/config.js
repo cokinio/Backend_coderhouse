ec905222b78b362563990fe302ebf1306a8ac0b7
Leandro Schlain <cokinio@gmail.com>
2023-08-17 12:21:27 -0300

    modificaciones para railway app

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,25 @@
-import dotenv from 'dotenv';
-import { Command } from 'commander';
-import { miLogger } from './logger.js';
+// import dotenv from 'dotenv';
+// import { Command } from 'commander';
 
-const program = new Command(); //Crea la instancia de comandos de commander.
+// const program = new Command(); //Crea la instancia de comandos de commander.
 
-program
-    .option('-d', 'Variable para debug', false)
-    .option('-p <port>', 'Puerto del servidor', 9090)
-    .option('--mode <mode>', 'Modo de trabajo', 'develop')
-program.parse();
+// program
+//     .option('-d', 'Variable para debug', false)
+//     .option('-p <port>', 'Puerto del servidor', 9090)
+//     .option('--mode <mode>', 'Modo de trabajo', 'develop')
+// program.parse();
 
-console.log("Mode Option: ", program.opts().mode);
+// console.log("Mode Option: ", program.opts().mode);
 
-const environment = program.opts().mode;
+// const environment = program.opts().mode;
 
-dotenv.config({
-    path: environment === "production" ? "./src/config/.env.production" : "./src/config/.env.development"
-});
+// dotenv.config({
+//     path: environment === "production" ? "./src/config/.env.production" : "./src/config/.env.development"
+// });
 
 export default {
-    port: process.env.PORT,
+    environment:"production",
+    port: process.env.PORT||8080,
     mongoUrl: process.env.MONGO_URL,
     sessionSecret: process.env.SESION_SECRET,
     gmailAccount: process.env.GMAIL_ACCOUNT,
