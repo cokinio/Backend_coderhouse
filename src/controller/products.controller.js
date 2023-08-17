01dc428c633c2197670a37fa00f4fc8b59270da4
Leandro Schlain <cokinio@gmail.com>
2023-08-17 12:36:07 -0300

    se trata de corregir error de railway

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -1,7 +1,7 @@
 import {getAllProducts,getProductUsingId,postNewProduct,deleteProduct,UpdateProductUsingId} from "../services/products.service.js"
 import EErrors from "../services/errors/errors-enum.js";
 import { generateUserErrorInfo } from "../services/errors/messages/product-creation-error.message.js";
-import CustomError from "../services/errors/CustomError.js";
+import {CustomError} from "../services/errors/CustomError.js";
 import { miLogger } from '../config/logger.js';
 
 export const getProducts =async (req,res)=>{
